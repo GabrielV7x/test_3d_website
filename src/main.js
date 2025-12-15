4b234002d3fe2fd9c07ea28bbd825f0733c97bcf
Frederic Feliciane <fredericfeliciane8@gmail.com>
2025-12-16 02:25:01 +0400

    orbit controls

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 
 // Setup
@@ -35,10 +36,29 @@ pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-scene.add(pointLight);
+// Helpers
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
 
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
 
 
 function animate() {
@@ -50,7 +70,7 @@ function animate() {
 
   // moon.rotation.x += 0.005;
 
-  // controls.update();
+  controls.update();
 
   renderer.render(scene, camera);
 }
